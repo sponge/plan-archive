5c30ba5d2c4313aa00c153c19ff6dc652a3b87df
sponge <sponge@d8d.org>
2024-07-13 17:15:06 -0400

    don't trim strings in web since i did it in the flat files

--- src/App.tsx
+++ src/App.tsx
@@ -57,7 +57,6 @@ const App: Component = () => {
     const resp = await fetch('/plan-archive/plans.json');
     let plans: PlanFile[] = await resp.json();
     // sort by time here so we can depend on this in any further operations
-    plans = plans.map(plan => { return {...plan, contents: plan.contents.trim()}})
     return plans.sort((a, b) => a.time - b.time);
   }
   const [plans] = createResource(fetchPlans, { initialValue: [] });
